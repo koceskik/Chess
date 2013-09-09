7274d4507d21c2b0a52d9d0f833c36383e1133a6
Kyle Koceski <koceskik@gmail.com>
2013-09-08 21:36:17 -0400

    Tidied up a few TODO comments

--- src/Player.java
+++ src/Player.java
@@ -8,7 +8,7 @@ public class Player implements Serializable {
 	public PieceColor color = null;
 	public ArrayList<Piece> pieceList = new ArrayList<Piece>();
 	public ArrayList<Piece> deadPieces = new ArrayList<Piece>();//holds all dead pieces
-	public ArrayList<Piece> heldPieces = new ArrayList<Piece>();//TODO: bughouse: add pieces in deadPieces, queue
+	public ArrayList<Piece> heldPieces = new ArrayList<Piece>();
 	public ArrayList<Piece> queuingPieces = new ArrayList<Piece>();
 	public Piece king = null;
 	public UUID id = UUID.randomUUID();
@@ -17,13 +17,14 @@ public class Player implements Serializable {
 	public Player partner = null;
 	
 	// This is incremented during bughouse games to denote that there's another board to the client 
-	public int gameCount = 0;//technically the number of games is this value+1. ISN'T THIS UNNECESSARY with the partner variable
+	public int gameCount = 0;//technically the number of games is this value+1
+							 //TODO: ISN'T THIS UNNECESSARY with the partner variable
 
 	public Player(PieceColor pc) {
 		this(pc, 0);
 	}
 
-	public Player(PieceColor pc, int gameID) {//TODO: add security here: unique ID or have server send unique ID each turn
+	public Player(PieceColor pc, int gameID) {
 		color = pc;
 		this.gameID = gameID;
 		pieceList.add(new Rook(this));
@@ -44,7 +45,6 @@ public class Player implements Serializable {
 	}
 	
 	public void pickupQueue() {
-		//TODO: does this need a lock?
 		for(Piece p : queuingPieces) {
 			heldPieces.add(p);
 		}
